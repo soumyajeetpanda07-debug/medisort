3f74c3b48bbccdf4f2b21496750fba73e5753d6b
Soumyajeet Panda <soumyajeet.panda07@gmail.com>
2026-09-22 21:08:31 +0530

    Fix FrontPage filename case

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import FrontPage from "./frontpage";
+import FrontPage from "./FrontPage";
 import HospitalDashboard from "./pages/hospital/HospitalDashboard";
 import CollectorDashboard from "./pages/collector/collectorDashboard";
 import AdminDashboard from "./pages/admin/AdminDashboard";
